6dc2a031268187bff883531e025934ce21b1796f
ushapatturaj <ushadevipatturaj@gmail.com>
2020-07-20 17:54:39 +0200

    matrix transpose

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -11,8 +11,9 @@ def create_matrix(number):
 
 
 def create_zero_matrix(row, column):
-    matrix = [([0]*column) for i in range(row)]
+    matrix = [([0] * column) for _ in range(row)]
     return matrix
+
 
 def print_matrix(matrix):
     print('The result is:')
@@ -46,6 +47,7 @@ def matrix_multiplication_with_constant(matrix, multiplier):
         multi_matrix.append(temp)
     print_matrix(multi_matrix)
 
+
 def matrices_multiplication(matrix_1, matrix_2):
     if len(matrix_1[0]) == len(matrix_2):
         result_matrix = create_zero_matrix(len(matrix_1), len(matrix_2[0]))
@@ -57,10 +59,40 @@ def matrices_multiplication(matrix_1, matrix_2):
     else:
         print('The operation cannot be performed.\n')
 
+
+def matrix_transpose(matrix, type_char):
+    new_matrix = []
+    if type_char == 1:
+        new_matrix = create_zero_matrix(len(matrix), len(matrix[0]))
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                new_matrix[i][j] = matrix[j][i]
+    elif type_char == 2:
+        for i in range(len(matrix) - 1, -1, -1):
+            temp = []
+            for j in range(len(matrix[0]) - 1, -1, -1):
+                temp.append(matrix[j][i])
+            new_matrix.append(temp)
+    elif type_char == 3:
+        for i in range(len(matrix)):
+            temp = []
+            for j in range(len(matrix[0]) - 1, -1, -1):
+                temp.append(matrix[i][j])
+            new_matrix.append(temp)
+    elif type_char == 4:
+        for i in range(len(matrix) - 1, -1, -1):
+            temp = []
+            for j in range(len(matrix[0])):
+                temp.append(matrix[i][j])
+            new_matrix.append(temp)
+    print_matrix(new_matrix)
+
+
 def play():
     run = True
     while run:
-        print("1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n0. Exit\n")
+        print("\n1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n"
+              "4. Transpose matrix\n0. Exit\n")
         play_choice = int(input("Your choice:"))
         if play_choice == 0:
             run = False
@@ -78,4 +110,11 @@ def play():
             matrix_a = create_matrix("first")
             matrix_b = create_matrix("second")
             matrices_multiplication(matrix_a, matrix_b)
+        elif play_choice == 4:
+            print("\n1. Main diagonal\n2. Side diagonal\n3. Vertical line\n4. Horizontal line")
+            transpose_type = int(input("Your choice:"))
+            matrix = create_matrix("")
+            matrix_transpose(matrix, transpose_type)
+
+
 play()
