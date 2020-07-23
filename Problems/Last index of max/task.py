536e8c2285a021448794a8e7bb6c4bd8a6f2e8d2
ushapatturaj <ushadevipatturaj@gmail.com>
2020-07-23 14:04:45 +0200

    last index of the max value in a list

--- Problems/Last index of max/task.py
+++ Problems/Last index of max/task.py
@@ -1,13 +1,12 @@
 def last_indexof_max(numbers):
     # write the modified algorithm here
     temp = 0
-    index = 0
-    for n in range(len(numbers)):
-        if temp <= numbers[n]:
-            temp = numbers[n]
-            index = n
+    for i, num in enumerate(numbers):
+        if temp <= num:
+            temp = num
+            index = i
         else:
             continue
     return index
 
-# print(last_indexof_max([8, 11, 15, 15, 15, 12]))
+print(last_indexof_max([8, 11, 15, 15, 15, 12]))
