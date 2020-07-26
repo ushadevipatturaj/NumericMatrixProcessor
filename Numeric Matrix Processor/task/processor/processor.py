1311a3bfa22d33538ddd334c42910ae35da34e25
ushapatturaj <ushadevipatturaj@gmail.com>
2020-07-26 20:08:49 +0200

    Inverse Matrix

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -1,3 +1,5 @@
+import math
+
 def create_matrix(number):
     row, column = map(int, input("Enter size of {} matrix:".format(number)).split(" "))
     print("Enter {} matrix:".format(number))
@@ -43,9 +45,13 @@ def matrix_multiplication_with_constant(matrix, multiplier):
         temp = []
         for j in range(len(matrix[0])):
             result = matrix[i][j] * multiplier
-            temp.append(result)
+            if result == 0.0:
+                temp.append(0)
+            else:
+                temp.append(result)
         multi_matrix.append(temp)
-    print_matrix(multi_matrix)
+    return multi_matrix
+
 
 
 def matrices_multiplication(matrix_1, matrix_2):
@@ -85,14 +91,91 @@ def matrix_transpose(matrix, type_char):
             for j in range(len(matrix[0])):
                 temp.append(matrix[i][j])
             new_matrix.append(temp)
-    print_matrix(new_matrix)
+    return new_matrix
+
+
+def matrix_determinant(matrix):
+    if len(matrix) >= 2 and len(matrix[0]) >= 2:
+        return find_cofactor(matrix)
+    else:
+        return matrix[0][0]
+
+
+def find_cofactor(matrix):
+    determinant = 0
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        determinant = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    else:
+        for i in range(len(matrix[0])):
+            temp = matrix[0][i]
+            cofactor_matrix = []
+            for x in range(len(matrix)):
+                inner = []
+                for y in range(len(matrix[0])):
+                    if x == 0:
+                        continue
+                    if y == i:
+                        continue
+                    elif x != 0 and y != i:
+                        inner.append(matrix[x][y])
+                if len(inner) > 0:
+                    cofactor_matrix.append(inner)
+            determinant += pow(-1, i) * (temp * find_cofactor(cofactor_matrix))
+    return determinant
+
+
+def find_adjoint(matrix):
+    inverse_cofactor = []
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    else:
+        for i in range(len(matrix)):
+            temp_cofactor = []
+            for j in range(len(matrix[0])):
+                temp = matrix[i][j]
+                cofactor_matrix = []
+                for x in range(len(matrix)):
+                    inner = []
+                    for y in range(len(matrix[0])):
+                        if x == i:
+                            continue
+                        if y == j:
+                            continue
+                        elif x != i and y != j:
+                            inner.append(matrix[x][y])
+                    if len(inner) > 0:
+                        cofactor_matrix.append(inner)
+                determinant = pow(-1, i) * find_adjoint(cofactor_matrix)
+                temp_cofactor.append(determinant)
+            inverse_cofactor.append(temp_cofactor)
+    return inverse_cofactor
+
+
+def matrix_inverse(matrix):
+    if len(matrix) <= 2 and len(matrix[0]) <= 2:
+        print("This matrix doesn't have an inverse.")
+        return 0
+    else:
+        determinant = matrix_determinant(matrix)
+        if determinant == 0:
+            print("This matrix doesn't have an inverse.")
+            return 0
+        cofactor = find_adjoint(matrix)
+        transposed_cofactor = matrix_transpose(cofactor, 1)
+        print(transposed_cofactor)
+        quotient = round(1.00 / determinant, 7)
+        multiplier = math.floor(abs(quotient) * 10 ** 4) / 10 ** 4
+        if quotient < 0:
+            multiplier = multiplier * -1
+        print(quotient, determinant, quotient)
+        return matrix_multiplication_with_constant(transposed_cofactor, multiplier)
 
 
 def play():
     run = True
     while run:
         print("\n1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n"
-              "4. Transpose matrix\n0. Exit\n")
+              "4. Transpose matrix\n5. Calculate a determinant\n6. Inverse matrix\n0. Exit\n")
         play_choice = int(input("Your choice:"))
         if play_choice == 0:
             run = False
@@ -105,7 +188,8 @@ def play():
             matrix_a = create_matrix("")
             n = input('Enter constant:')
             n = int(n) if '.' not in n else float(n)
-            matrix_multiplication_with_constant(matrix_a, n)
+            multi_matrix = matrix_multiplication_with_constant(matrix_a, n)
+            print_matrix(multi_matrix)
         elif play_choice == 3:
             matrix_a = create_matrix("first")
             matrix_b = create_matrix("second")
@@ -114,7 +198,16 @@ def play():
             print("\n1. Main diagonal\n2. Side diagonal\n3. Vertical line\n4. Horizontal line")
             transpose_type = int(input("Your choice:"))
             matrix = create_matrix("")
-            matrix_transpose(matrix, transpose_type)
+            new_matrix = matrix_transpose(matrix, transpose_type)
+            print_matrix(new_matrix)
+        elif play_choice == 5:
+            matrix = create_matrix("")
+            determinant = matrix_determinant(matrix)
+            print("The result is:\n", determinant, sep="")
+        elif play_choice == 6:
+            matrix = create_matrix("")
+            inverse_matrix = matrix_inverse(matrix)
+            print_matrix(inverse_matrix)
 
 
 play()
